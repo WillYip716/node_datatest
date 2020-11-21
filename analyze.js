1c758076ef5c3b8467e36afbfd65c8c43ebf2ce6
WillYip716 <william.yip.716@gmail.com>
2020-11-20 17:03:34 -0800

    testing data reading and writing

---
+++ analyze.js
@@ -0,0 +1,20 @@
+var fs = require("fs");
+var d3 = require("d3");
+var _  = require("lodash");
+
+fs.readFile("data/animals.tsv", "utf8", function(error, data) {
+  data = d3.tsvParse(data);
+  console.log(JSON.stringify(data));
+
+  var maxWeight = d3.max(data, function(d) { return d.avg_weight; });
+  console.log(maxWeight);
+
+  var bigAnimals = data.filter(function(d) { return d.avg_weight > 300; });
+  bigAnimalsString = JSON.stringify(bigAnimals);
+
+  fs.writeFile("big_animals.json", bigAnimalsString, function(err) {
+    console.log("file written");
+  });
+});
+
+
