4c2b491759a0c2992866e384b9dbb12797b42e49
WillYip716 <william.yip.716@gmail.com>
2020-11-24 15:45:29 -0800

    finished dates grabber, added spreads grabber

--- getspreads.js
+++ getspreads.js
@@ -2,22 +2,36 @@ const puppeteer = require('puppeteer');
 const $ = require('cheerio');
 var fs = require("fs");
 
-async function findDates() {
+async function grabSpreads(){
     // prepare for headless chrome
-    let allDates = [];
-    let years = ["2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019"];
+    let spreadsinfo = [];
+    let dates = [];
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     let url;
-    let parser = new RegExp(/([0-9]*)-([0-9]*)-([0-9]*)/);
-    // set user agent (override the default headless User Agent)
+
+    /*
+    {
+        date: 20121014,
+        Away team: Cincinnati,
+        Home team: Cleveland,
+        away score: 24,
+        home score: 34,
+        away opening spread: -1,
+        home opening spread: +1,
+        away closing spread: -1,
+        home closing spread: +1,
+        opening total: 44,
+        closing total: 41.5,
+    }*/
+
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
     
     for (let i = 0; i < years.length; i++) {
-        url = "https://www.pro-football-reference.com/years/" + years[i] + "/games.htm";
+        url = "https://www.sportsbookreview.com/betting-odds/nfl-football/merged/?date=" + dates[i].replaceAll("-","");
         await page.goto(url);
         const content = await page.content();
-        $('tbody tr', content).each((index,element) => {
+        $('div.eventMarketGridContainer-3QipG', content).each((index,element) => {
             let field = $(element).find("td");
             if(field[1]&&(parser.test(field[1].attribs.csk))){
                 if(!allDates.includes(field[1].attribs.csk)){
@@ -28,11 +42,11 @@ async function findDates() {
         });
     }
 
-    fs.writeFile("./data/dates.txt", JSON.stringify(allDates),function(err){
+    fs.writeFile("./data/spreadinfo.txt", JSON.stringify(spreadsinfo),function(err){
         console.log("file written");
     })
 
     await browser.close();
 }
 
-findDates();
+
