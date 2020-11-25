b92e21cb98225898c13bea658b67a8a7406beba8
WillYip716 <william.yip.716@gmail.com>
2020-11-25 15:53:59 -0800

    grab relevant data and create game object to push to database

--- getspreads.js
+++ getspreads.js
@@ -5,48 +5,69 @@ var fs = require("fs");
 async function grabSpreads(){
     // prepare for headless chrome
     let spreadsinfo = [];
-    let dates = [];
+    let dates = ["20121014"];
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     let url;
 
-    /*
-    {
-        date: 20121014,
-        Away team: Cincinnati,
-        Home team: Cleveland,
-        away score: 24,
-        home score: 34,
-        away opening spread: -1,
-        home opening spread: +1,
-        away closing spread: -1,
-        home closing spread: +1,
-        opening total: 44,
-        closing total: 41.5,
-    }*/
-
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
     
-    for (let i = 0; i < years.length; i++) {
-        url = "https://www.sportsbookreview.com/betting-odds/nfl-football/merged/?date=" + dates[i].replaceAll("-","");
-        await page.goto(url);
+    for (let i = 0; i < dates.length; i++) {
+        url = "https://www.sportsbookreview.com/betting-odds/nfl-football/merged/?date=" + dates[i];
+        await page.goto(url, { waitUntil: 'networkidle0' });
         const content = await page.content();
         $('div.eventMarketGridContainer-3QipG', content).each((index,element) => {
-            let field = $(element).find("td");
-            if(field[1]&&(parser.test(field[1].attribs.csk))){
-                if(!allDates.includes(field[1].attribs.csk)){
-                    allDates.push(field[1].attribs.csk);
-                }   
+            let game = {};
+            game.date = dates[i];
+
+            let teams = $(element).find("span.participantBox-3ar9Y");
+            game.away_team = $(teams[0]).text();
+            game.home_team = $(teams[1]).text();
+
+            let scores = $(element).find("div.finalScore-156Hx > div");
+            game.away_score = $(scores[0]).text();
+            game.home_score = $(scores[1]).text();
+
+            let lines = $(element).find("span.adjust-1uDgI");
+
+            if($(lines[0]).text().charAt(0) == "-"){
+                game.away_open = $(lines[0]).text().replace("½",".5");
+                game.home_open = "+" + $(lines[0]).text().slice(1).replace("½",".5");
+                game.total_open = $(lines[1]).text().replace("½",".5");
             }
+            else{
+                game.away_open = "+" + $(lines[1]).text().slice(1).replace("½",".5");
+                game.home_open = $(lines[1]).text().replace("½",".5");
+                game.total_open = $(lines[0]).text().replace("½",".5");
+            }
+
+            if($(lines[2]).text().charAt(0) == "-"){
+                game.away_close = $(lines[2]).text().replace("½",".5");
+                game.home_close = "+" + $(lines[2]).text().slice(1).replace("½",".5");
+                game.total_close = $(lines[3]).text().replace("½",".5");
+            }
+            else{
+                game.away_close = "+" + $(lines[3]).text().slice(1).replace("½",".5");
+                game.home_close = $(lines[3]).text().replace("½",".5"); 
+                game.total_close = $(lines[2]).text().replace("½",".5");
+            }
+
+
+            spreadsinfo.push(game);
+
+            
 
         });
     }
 
-    fs.writeFile("./data/spreadinfo.txt", JSON.stringify(spreadsinfo),function(err){
+    console.log(spreadsinfo);
+    /*fs.writeFile("./data/spreadinfo.txt", JSON.stringify(spreadsinfo),function(err){
         console.log("file written");
-    })
+    })*/
 
     await browser.close();
 }
 
+
+grabSpreads();
 
