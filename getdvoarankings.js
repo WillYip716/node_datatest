affbb5c56247fe811f01ea1437b3131e29d4a2ae
WillYip716 <william.yip.716@gmail.com>
2020-12-01 15:38:50 -0800

    fine tuning get

--- getdvoarankings.js
+++ getdvoarankings.js
@@ -11,10 +11,12 @@ async function getdvoaratings() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     let url;
+    let parser = new RegExp(/(wei|dave)/mi);
     // set user agent (override the default headless User Agent)
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
     
     for (let i = 0; i < years.length; i++) {
+        let yearsData = [];
         for (let j = 0; j < week.length; j++) {
             url = "https://www.footballoutsiders.com/dvoa-ratings/"+years[i]+"/week-"+week[j]+"-dvoa-ratings"
             await page.goto(url, { waitUntil: 'networkidle0' });
@@ -22,15 +24,35 @@ async function getdvoaratings() {
             $('tbody', content).each((index,element) => {
                 let field = $(element).find("tr");
                 //for (let k = 0; k < field.length; k++) {
-                console.log("this is index " + index + " and tr length is "+ field.length);
+                //console.log("this is index " + index + " and tr length is "+ field.length);
+                if(parser.test($(field[0]).text())&&field.length>31){
+                    let keys = ["holder"];
+
+                    field.each((count,ele) => {
+                        let cell = $(ele).find("td");
+                        let data = {"week": week[j]};
+                        for(let k = 1; k < cell.length; k++){
+                            if(count == 0){
+                                keys.push($(cell[k]).text());
+                            }
+                            else{
+                                data[keys[k].replace(/\n/g,"")] = $(cell[k]).text();
+                            }
+                        }
+                        if(count > 0 && (data[keys[2].replace(/\n/g,"")].indexOf("\n") < 0)){
+                            yearsData.push(data);
+                        }
+                    })
+                }
             });
-            
+            console.log("week " + week[j] + " finished");
         }
+        fs.writeFile("./data/"+years[i]+"dvoaRanking.txt", JSON.stringify(yearsData),function(err){
+            console.log("file written " + years[i]);
+        })
     }
 
-    /*fs.writeFile("./data/dates.txt", JSON.stringify(allDates),function(err){
-        console.log("file written");
-    })*/
+    
 
     await browser.close();
 }
