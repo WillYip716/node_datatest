81222601afa423af8efce40a6306a12bf885a094
WillYip716 <william.yip.716@gmail.com>
2020-12-08 16:04:38 -0800

    update cleaner to standardize keys

--- datacleaner.js
+++ datacleaner.js
@@ -1,31 +1,23 @@
 var fs = require("fs");
 
 let years = ["2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019"];
-let anomalies = [];
+//let years = ["2015"];
 
-for (let i = 0; i < years.length; i++) {
-  var text = fs.readFileSync("./data/"+years[i]+"dvoaRanking.txt");
-  let spreads = JSON.parse(text);
-  console.log(spreads.length);
-  for (let j = 0; j < spreads.length; j++) {
-      if(Object.keys(spreads[j]).length !== 15){
-        //console.log(spreads[j]);
-        anomalies.push(spreads[j]);
-      }
-
-      /*if((parseFloat(spreads[j].total_open) < 25)||(parseFloat(spreads[j].total_close) < 25)){
-          anomalies.push(spreads[j]);
-      }*/
-  }
-
-  /*fs.writeFile("./data/"+years[i]+"anomalies.txt", JSON.stringify(anomalies),function(err){
-    console.log("anomalies " + years[i] + " written");
-  })*/
-
+function cleanerAndNormalize() {
+  //let parser = /\\t/gi;
+  let parser = /TOTALVOA|ACTUALDVOA/gi;
+  for (let i = 0; i < years.length; i++) {
+    var readFile = fs.readFileSync("./data/"+years[i]+"dvoaRanking.txt");
+    var text = readFile.toString();
+    text = text.replace(parser,"TOTALDVOA");
+    //console.log(spreads.length);
+    
+  
+    fs.writeFile("./data/"+years[i]+"dvoaRanking.txt", text,function(err){
+      console.log("anomalies " + years[i] + " written");
+    })
+  
+  } 
 }
 
-
-
-/*fs.writeFile("./data/anomalies.txt", JSON.stringify(anomalies),function(err){
-    console.log("anomalies file written");
-})*/
+cleanerAndNormalize();
--- normalizer.js
+++ normalizer.js
@@ -1,24 +1,54 @@
 var fs = require("fs");
 
+let years = ["2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019"];
+//let years = ["2015"];
 
+function teamNames(yearsToCheck) {
+  let finalCount = {};
+  for (let i = 0; i < yearsToCheck.length; i++) {
+    var text = fs.readFileSync("./data/"+yearsToCheck[i]+"dvoaRanking.txt");
+    finalCount = {};
+    let spreads = JSON.parse(text);
+    spreads.forEach(element => {
+      if (element["TEAM"] in finalCount){
+          finalCount[element["TEAM"]] = parseInt(finalCount[element["TEAM"]]) + 1;
+      }
+      else{
+          finalCount[element["TEAM"]] = 1;
+      }
+    });
+    console.log("Final Count for year " + yearsToCheck[i])
+    console.log(finalCount);
+  } 
+}
 
-let years = ["2007"];
-let finalCount = {};
-
-for (let i = 0; i < years.length; i++) {
-  var text = fs.readFileSync("./data/"+years[i]+"dvoaRanking.txt");
-  let spreads = JSON.parse(text);
-  spreads.forEach(element => {
-    if (element["TEAM"] in finalCount){
-        finalCount[element["TEAM"]] = parseInt(finalCount[element["TEAM"]]) + 1;
-    }
-    else{
-        finalCount[element["TEAM"]] = 1;
-    }
-  });
-
+function checkKeys(yearsToCheck) {
+  let finalCount = {};
+  for (let i = 0; i < yearsToCheck.length; i++) {
+    var text = fs.readFileSync("./data/"+yearsToCheck[i]+"dvoaRanking.txt");
+    //finalCount = {};
+    let spreads = JSON.parse(text);
+    spreads.forEach(element => {
+        let keys = Object.keys(element);
+        for (let j = 0; j < keys.length; j++) {
+            if (keys[j] in finalCount){
+                finalCount[keys[j]] = parseInt(finalCount[keys[j]]) + 1;
+            }
+            else{
+                finalCount[keys[j]] = 1;
+            }
+          
+        }
+    });
+    //console.log("Final Count for year " + yearsToCheck[i])
+    //console.log(finalCount);
+  }
+  console.log(finalCount);
 }
 
 
-console.log(finalCount);
-console.log("number of teams is " + Object.keys(finalCount).length);
+
+//teamNames(years);
+checkKeys(years);
+//console.log(finalCount);
+//console.log("number of teams is " + Object.keys(finalCount).length);
