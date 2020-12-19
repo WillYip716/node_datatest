df5480894377ee07e9aaff67526fe1974f6cafe6
WillYip716 <william.yip.716@gmail.com>
2020-12-19 15:50:59 -0800

    added functions to generate weeks spreads

--- spreads_analyzer.js
+++ spreads_analyzer.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var XLSX = require('xlsx')
-
+const csv = require('csv-parser');
 
 
 
@@ -9,23 +9,19 @@ var XLSX = require('xlsx')
 //SD 30 	-26.8% 	19 	-1.1% 	22 	7.3% 	32 	-13.5%
 //LV 22 	-8.7% 	15 	1.5% 	28 	10.5% 	12 	1.5%
 
-let testgame = {
-  "vTOTAL.RNK" : 30,
-  "vTOTAL.DVOA": "-26.8%",
-  "hTOTAL.RNK" : 22,
-  "hTOTAL.DVOA": "-8.7%",
-  "vOFF.RNK" : 19,
-  "vOFF.DVOA": "-1.1%",
-  "hOFF.RNK" : 15,
-  "hOFF.DVOA": "1.5%",
-  "vDEF.RNK" : 22,
-  "vDEF.DVOA": "7.3%",
-  "hDEF.RNK" : 28,
-  "hDEF.DVOA": "10.5%",
-  "vST.RNK" : 32,
-  "vST.DVOA": "-13.5%",
-  "hST.RNK" : 12,
-  "hST.DVOA": "1.5%"
+
+
+
+function getallgames(){
+    let years = ["2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019"];
+    for (let i = 0; i < years.length; i++) {
+      let text = fs.readFileSync("./data/mergedstats" + years[i] +".txt");
+      let spreads = JSON.parse(text);
+
+      console.log(spreads.length);
+      
+    }
+
 }
 
 
@@ -85,7 +81,7 @@ function analyzeSpread(game){
           }
       });
 
-      console.log(datatable);
+      //console.log(datatable);
       console.log(datatable.length);
 
       console.log("avg score of visitors : " + (avgvscore/datatable.length));
@@ -106,18 +102,18 @@ function analyzeSpread(game){
 function fullcheck(element,game){
     //console.log(element);
     /*
-    let vtotalcheck = checker(element,game,"vTOTAL.DVOA",3);
-    let htotalcheck = checker(element,game,"hTOTAL.DVOA",3);
+    let vtotalcheck = checker(element,game,"vTOTAL.RNK",32);
+    let htotalcheck = checker(element,game,"hTOTAL.RNK",32);
     */
 
-    let voffcheck = checker(element,game,"vOFF.DVOA",6);
-    let hoffcheck = checker(element,game,"hOFF.DVOA",6);
+    let voffcheck = checker(element,game,"vOFF.DVOA",7);
+    let hoffcheck = checker(element,game,"hOFF.DVOA",7);
 
-    let vdefcheck = checker(element,game,"vDEF.DVOA",6);
-    let hdefcheck = checker(element,game,"hDEF.DVOA",6);
+    let vdefcheck = checker(element,game,"vDEF.DVOA",7);
+    let hdefcheck = checker(element,game,"hDEF.DVOA",7);
   
-    let vstcheck = checker(element,game,"vST.DVOA",6);
-    let hstcheck = checker(element,game,"hST.DVOA",6);
+    let vstcheck = checker(element,game,"vST.DVOA",7);
+    let hstcheck = checker(element,game,"hST.DVOA",7);
     //if(vtotalcheck&&htotalcheck&&voffcheck&&hoffcheck&&vdefcheck&&hdefcheck&&vstcheck&&hstcheck){
     if(voffcheck&&hoffcheck&&vdefcheck&&hdefcheck&&vstcheck&&hstcheck){
     //if(vtotalcheck&&htotalcheck){
@@ -167,10 +163,97 @@ function spreadResult(game){
     return results;
 }
 
+function getStandardDeviation (array) {
+  const n = array.length
+  const mean = array.reduce((a, b) => a + b) / n
+  return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+}
 
 
+function weeksspread(){
+    
 
-analyzeSpread(testgame);
+    const thisweekrankings = []
+    fs.createReadStream('./data/2020 Team DVOA Ratings Overall.csv')
+      .pipe(csv())
+      .on('data', (r) => {
+        thisweekrankings.push(r);        
+      })
+      .on('end', () => {
+        getweeksinfo(thisweekrankings);
+    })    
+
+}
+
+function getweeksinfo(dvoarankings){
+    const teamkeys = {
+      'Cincinnati Bengals':'CIN',
+      'Cleveland Browns':'CLE',
+      'New York Giants':'NYG',
+      'Chicago Bears':'CHI',
+      'Atlanta Falcons':'ATL',
+      'Dallas Cowboys':'DAL',
+      'Detroit Lions':'DET',
+      'Green Bay Packers':'GB',
+      'Minnesota Vikings':'MIN',
+      'Indianapolis Colts':'IND',
+      'Buffalo Bills':'BUF',
+      'Miami Dolphins':'MIA',
+      'San Francisco 49ers':'SF',
+      'New York Jets':'NYJ',
+      'Los Angeles Rams':'LAR',
+      'Philadelphia Eagles':'PHI',
+      'Denver Broncos':'DEN',
+      'Pittsburgh Steelers':'PIT',
+      'Carolina Panthers':'CAR',
+      'Tampa Bay Buccaneers':'TB',
+      'Jacksonville Jaguars':'JAX',
+      'Tennessee Titans':'TEN',
+      'Washington Football Team':'WAS',
+      'Washington':'WAS',
+      'Arizona Cardinals':'ARI',
+      'Baltimore Ravens':'BAL',
+      'Houston Texans':'HOU',
+      'Kansas City Chiefs':'KC',
+      'Los Angeles Chargers':'LAC',
+      'New England Patriots':'NE',
+      'Seattle Seahawks':'SEA',
+      'New Orleans Saints':'NO',
+      'Las Vegas Raiders':'LV'
+    };
+
+    let thisweekslines = "Los%20Angeles%20Chargers%20at%20Las%20Vegas%20Raiders%20%28-3.5%2C%2052.0%29%0ABuffalo%20Bills%20at%20Denver%20Broncos%20%28+6.0%2C%2049.5%29%0ACarolina%20Panthers%20at%20Green%20Bay%20Packers%20%28-8.5%2C%2051.5%29%0ATampa%20Bay%20Buccaneers%20at%20Atlanta%20Falcons%20%28+6.0%2C%2049.5%29%0ASan%20Francisco%2049ers%20at%20Dallas%20Cowboys%20%28+3.0%2C%2045.0%29%0ADetroit%20Lions%20at%20Tennessee%20Titans%20%28-10.5%2C%2051.5%29%0AHouston%20Texans%20at%20Indianapolis%20Colts%20%28-7.5%2C%2051.0%29%0ANew%20England%20Patriots%20at%20Miami%20Dolphins%20%28-2.0%2C%2041.5%29%0AChicago%20Bears%20at%20Minnesota%20Vikings%20%28-3.0%2C%2046.5%29%0ASeattle%20Seahawks%20at%20Washington%20%28+5.5%2C%2044.5%29%0AJacksonville%20Jaguars%20at%20Baltimore%20Ravens%20%28-13.0%2C%2047.5%29%0ANew%20York%20Jets%20at%20Los%20Angeles%20Rams%20%28-17.5%2C%2044.0%29%0APhiladelphia%20Eagles%20at%20Arizona%20Cardinals%20%28-6.5%2C%2049.5%29%0AKansas%20City%20Chiefs%20at%20New%20Orleans%20Saints%20%28+3.0%2C%2051.5%29%0ACleveland%20Browns%20at%20New%20York%20Giants%20%28+6.0%2C%2044.5%29%0APittsburgh%20Steelers%20at%20Cincinnati%20Bengals%20%28+13.0%2C%2040.5%29";
+
+    let linesArr = unescape(thisweekslines).split("\n");
+    let linereader = new RegExp(/([A-Za-z0-9 ]*)\sat\s([A-Za-z0-9 ]*)\s\((\+|\-)([0-9.]*),\s([0-9.]*)\)/);
+    for(var i = 0; i <linesArr.length;i++){
+      let game = {"visitor": teamkeys[linesArr[i].match(linereader)[1]], "home":teamkeys[linesArr[i].match(linereader)[2]]};
+      let vdvoa = dvoarankings.find(element=> element["Team"]==game["visitor"]);
+      let hdvoa = dvoarankings.find(element=> element["Team"]==game["home"]);
+      game["vTOTAL.RNK"] = vdvoa['Weighted DVOA Rank'];
+      game["vTOTAL.DVOA"] = vdvoa['Weighted DVOA'];
+      game["vOFF.RNK"] = vdvoa['Offense DVOA Rank'];
+      game["vOFF.DVOA"] = vdvoa['Offense DVOA'];
+      game["vDEF.RNK"] = vdvoa['Defense DVOA Rank'];
+      game["vDEF.DVOA"] = vdvoa['Defense DVOA'];
+      game["vST.RNK"] = vdvoa['Special Teams DVOA Rank'];
+      game["vST.DVOA"] = vdvoa['Special Teams DVOA'];
+      game["hTOTAL.RNK"] = hdvoa['Weighted DVOA Rank'];
+      game["hTOTAL.DVOA"] = hdvoa['Weighted DVOA'];
+      game["hOFF.RNK"] = hdvoa['Offense DVOA Rank'];
+      game["hOFF.DVOA"] = hdvoa['Offense DVOA'];
+      game["hDEF.RNK"] = hdvoa['Defense DVOA Rank'];
+      game["hDEF.DVOA"] = hdvoa['Defense DVOA'];
+      game["hST.RNK"] = hdvoa['Special Teams DVOA Rank'];
+      game["hST.DVOA"] = hdvoa['Special Teams DVOA'];
+      console.log(game["visitor"] + " at " + game["home"]);
+      analyzeSpread(game);
+    }
+}
+
+//getallgames();
+//
+weeksspread();
 
 
 /*
